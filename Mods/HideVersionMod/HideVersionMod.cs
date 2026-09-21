b76f8cebd4f471a735515e63b3db399870173138
karan <karanb1@umbc.edu>
2026-09-21 12:02:42 -0400

    redo using template

---
+++ Mods/HideVersionMod/HideVersionMod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HarmonyLib;
+
+namespace HideVersionMod
+{
+    // ModBehaviour will be instantiated and attached as a component in a container game object named <Your Mod Id>
+    // Multiple ModBehaviours in your mod will share the same container.
+    public class HideVersionMod : ModBehaviour
+    {
+        private void Awake()
+        {
+            // Metadata of your mod is stored in this.about
+            Debug.Log("HideVersionMod loaded " + mod.metadata.id);
+
+            // Hide version UI
+            GameObject versionNumber = GameObject.Find("Version Number");
+            if (versionNumber != null)
+            {
+                versionNumber.SetActive(false);
+            }
+            
+            // If you need to patch with Harmony, you can use this.harmony to access the Harmony instance for your mod.
+            // It will be created with your mod's id automatically, the first time you access the property.
+            harmony.PatchAll();
+        }
+
+        private void OnDestroy()
+        {
+            // Make sure you clean up properly to support Live Reload.
+            Debug.Log("HideVersionMod closing " + mod.metadata.id);
+            harmony.UnpatchAll();
+        }
+    }
+}
